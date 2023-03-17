a1d4f03b428540abd8042cd830e452dde28ecd41
Bankass77 <amguindo77@gmail.com>
2023-03-17 21:44:39 +0100

    create new repo with submodule

---
+++ userservice/userservice/src/main/java/ml/densagou/userservice/App.java
@@ -0,0 +1,13 @@
+package ml.densagou.userservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
